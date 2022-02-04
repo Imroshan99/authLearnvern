4373ddf3458cac60901c761e8684774df9b01e8b
Imroshan99 <roshantambe1999@gmail.com>
2022-02-04 16:55:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import Auth from "../src/components/Auth.js";
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+          <Auth/>
+    </div>
+  );
+}
+
+export default App;
